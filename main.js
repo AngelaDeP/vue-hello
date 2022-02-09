5b577f33e474f626dea49ee2ab4253919b2a27cb
Angela De Pasquale <angeladepasquale_@libero.it>
2022-02-09 17:19:51 +0100

    Effettuato l'esercizio

---
+++ main.js
@@ -0,0 +1,10 @@
+const root = new Vue (
+    {
+        el: "#dinamic",
+        data: {
+            nome: '',
+            cognome: '',
+            immagine: 'https://picsum.photos/seed/picsum/500/300',
+        },
+    }
+);
